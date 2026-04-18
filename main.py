cfb7861fd7679f116ab9df9d37131e37aebb6581
lamnguyenvan161 <lam.nguyenvan@spxexpress.com>
2026-04-18 19:14:29 +0700

    main.py

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import gspread
 from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
-SHEET_ID = "1Y31SSWQ18svGk2cdTZzpVFR8kCk2yVPlGDFyVOFrAx4"  # <-- thay ID sheet
+SHEET_ID = "DAN_ID_SHEET_CUA_BAN"
 SHEET_NAME = "Sheet1"
 
 def connect_sheet():
     raw = os.getenv("GOOGLE_CREDENTIALS")
 
-    if not raw:
-        raise Exception("❌ GOOGLE_CREDENTIALS is None (chưa truyền env)")
+    # ✅ nếu có secret
+    if raw:
+        creds_dict = json.loads(raw)
 
-    creds_dict = json.loads(raw)
+    # ✅ nếu KHÔNG có secret → dùng file local
+    else:
+        print("⚠️ Không có secret → dùng credentials.json")
+        with open("credentials.json", "r") as f:
+            creds_dict = json.load(f)
 
     scope = [
         "https://spreadsheets.google.com/feeds",
@@ -25,10 +30,12 @@ def connect_sheet():
 
     return client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
 
+
 def main():
     sheet = connect_sheet()
     sheet.update_acell("A1", "OK " + datetime.now().strftime("%H:%M:%S"))
     print("✅ DONE")
+
 
 if __name__ == "__main__":
     main()
