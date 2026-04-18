9a8dedc59c383555a146eb5c4e284d4eb389c4c1
lamnguyenvan161 <lam.nguyenvan@spxexpress.com>
2026-04-18 18:30:25 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import json
+import gspread
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+
+SHEET_ID = "1Y31SSWQ18svGk2cdTZzpVFR8kCk2yVPlGDFyVOFrAx4"
+SHEET_NAME = "Sheet1"
+
+def connect_sheet():
+    creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+
+    return client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
+
+def main():
+    sheet = connect_sheet()
+    sheet.update_acell("A1", "OK " + datetime.now().strftime("%H:%M:%S"))
+    print("DONE")
+
+if __name__ == "__main__":
+    main()
